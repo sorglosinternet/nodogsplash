347ad4c765016e1d28e17a9f4ce2461692698679
Alexander Couzens <lynxis@fe80.eu>
2026-01-12 21:24:06 +0100

    auth.c: ignore state transistions to the same state

--- src/auth.c
+++ src/auth.c
@@ -79,7 +79,7 @@ int auth_change_state(t_client *client, const unsigned int new_state, const char
 	const unsigned int state = client->fw_connection_state;
 
 	if (state == new_state) {
-		return -1;
+		return 0;
 	} else if (state == FW_MARK_PREAUTHENTICATED) {
 		if (new_state == FW_MARK_AUTHENTICATED) {
 			fw_gops.authenticate(client);
